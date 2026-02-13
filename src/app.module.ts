4dc1c471d6250094997f964ff2bc6d7cdb7cfcba
xaxxoo <bnahmad83@gmail.com>
2026-02-13 23:05:55 +0100

    new modules added using the g flag

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { UsersModule } from './users/users.module';
     }),
     AuthModule,
     UsersModule,
+    ProductsModule,
   ],
 })
 export class AppModule {}
